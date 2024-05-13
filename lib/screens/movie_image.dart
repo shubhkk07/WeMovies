6b0aec9a7d2104e99978afc0445b6db9f0adb2dd
shubhkk07 <shubhkk07@gmail.com>
2024-05-13 17:46:27 +0530

    fix

--- lib/screens/movie_image.dart
+++ lib/screens/movie_image.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:path/path.dart' as path;
+import 'package:movieapp/screens/custom_clipper.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class MovieImage extends StatefulWidget {
   static const url = 'https://image.tmdb.org/t/p/w500';
@@ -24,7 +27,6 @@ class _MovieImageState extends State<MovieImage> {
   }
 
   saveImageToLocalStorage() async {
-    print(widget.imagePath);
     final url = MovieImage.url + widget.imagePath;
     var response = await get(Uri.parse(url));
     Directory directory = await getApplicationCacheDirectory();
@@ -55,11 +57,18 @@ class _MovieImageState extends State<MovieImage> {
               fit: BoxFit.cover,
               height: widget.size.height,
               width: widget.size.width,
-              errorBuilder: (context, error, stackTrace) => CachedNetworkImage(
-                  fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) => Container(
+                  decoration: const BoxDecoration(color: Colors.white),
                   height: widget.size.height,
                   width: widget.size.width,
-                  imageUrl: MovieImage.url + widget.imagePath),
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Icon(
+                      Icons.no_photography,
+                      size: 40,
+                    ),
+                  )),
             );
           } else {
             return CachedNetworkImage(
